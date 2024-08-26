d85da32c84b5b5e7a448cf80cf7322dcafba0ba9
Denil Parada <paradadeniljose@gmail.com>
2024-08-26 12:48:52 -0600

    Se creo el ArrayList de de estudiantes

--- Gestionador.java
+++ Gestionador.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class Gestionador {
+    private ArrayList<Estudiante> estudiantes;
+
+    public Gestionador(ArrayList<Estudiante> estudiante) {
+        this.estudiantes = estudiante;
+    }
+
+    public ArrayList<Estudiante> getEstudiante() {
+        return estudiantes;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiantes.add(estudiante);
+    }
     
 }
