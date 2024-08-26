a9935b504a4867d091592ad89612135a91f3b69b
JorgeVilledaS <vil24932@uvg.edu.gt>
2024-08-26 12:27:00 -0600

    otro

--- Estudiante.java
+++ Estudiante.java
@@ -1,12 +1,55 @@
-import java.util.ArrayList;
-
 public class Estudiante {
     private String nombre;
     private String apellido;
-    private int id;
-    private String nacimiento;
-    private String email;
-    private ArrayList<NotaEx> grades;
+    private int ID;
+    private String cumple;
+    private String correo;
+
+    public Estudiante(String nombre, String apellido, int iD, String cumple, String correo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        ID = iD;
+        this.cumple = cumple;
+        this.correo = correo;
+    }
+    
+    public String getApellido() {
+        return apellido;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public String getCumple() {
+        return cumple;
+    }
+    public int getID() {
+        return ID;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public void setCumple(String cumple) {
+        this.cumple = cumple;
+    }
+    public void setID(int iD) {
+        ID = iD;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
 
     
-}   
+
+
+
+    
+}
