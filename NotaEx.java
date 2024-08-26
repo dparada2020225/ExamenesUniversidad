f9619934c0cfe52ca8562a6436befb30ebb0e233
Denil Parada <paradadeniljose@gmail.com>
2024-08-26 12:14:45 -0600

    metodos de NotasEx

--- NotaEx.java
+++ NotaEx.java
@@ -1,5 +1,31 @@
 public class NotaEx {
     private String materia;
     private float nota;
-    
+
+    public NotaEx(String materia, float nota) {
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "NotaEx [materia=" + materia + ", nota=" + nota + ", toString()=" + super.toString() + "]";
+    }
+
 }
